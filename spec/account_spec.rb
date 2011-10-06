3b56bf161a37a67cf6f91a41c7fddc06bfe3c2c9
dummy@d.com <pydipatr@uf040.cs.man.ac.uk>
2011-10-06 15:24:43 +0100

    balance passess it's first test after instaniating an account which contains 0.0

---
+++ spec/account_spec.rb
@@ -0,0 +1,10 @@
+require 'rspec'
+require 'account'
+
+describe Account do
+
+  it "should initially have a balance of zero" do
+      account = Account.new
+    account.balance.should == 0.0
+  end
+end
